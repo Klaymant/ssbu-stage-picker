5aa7d8f9353e70c76e64828acd5df3c376976277
klaymant <clement.gauthier@protonmail.com>
2024-06-16 23:17:29 +0200

    Apply a border color to hovered non selected stages

--- src/app/StageSelectionList.tsx
+++ src/app/StageSelectionList.tsx
@@ -18,7 +18,7 @@ export function StageSelectionList({ stages, action, getDisableState }: Props) {
 function getStageClasses(stage: Stage): string {
   switch(stage.state) {
     case 'none':
-      return 'border-white';
+      return 'border-white hover:border-purple-500';
     case 'banned':
       return 'border-red-500';
     case 'picked':
