e35f7eb3fcfdc7392f0b49d8c59b19bdfa96eccd
klaymant <clement.gauthier@protonmail.com>
2024-06-16 23:34:18 +0200

    Switch to counterpick phase mode when lauching next game

--- src/app/StageSelection.tsx
+++ src/app/StageSelection.tsx
@@ -4,12 +4,19 @@ import { BaseButton } from "./BaseButton";
 import { ChosenStageView } from "./ChosenStageView";
 import { GamePhaseInstructions } from "./GamePhaseInstructions";
 import { StageSelectionList } from "./StageSelectionList";
+import { useAppContext } from "./contexts/AppProvider";
 import { useStagesContext } from "./contexts/StagesProvider";
 
 export function StageSelection() {
   const { stages, gamePhase, gamePhaseInstructions, action, reset, getDisableState } = useStagesContext();
   const chosenStage = stages.find((stage) => stage.state === 'valided');
   const sectionBaseClasses = 'flex justify-around flex-wrap gap-x-2 p-1 sm:p-4';
+  const { setSetPhase } = useAppContext();
+
+  function handleNextGame() {
+    setSetPhase('counterPick');
+    reset();
+  }
 
   return (
     <>
@@ -18,7 +25,7 @@ export function StageSelection() {
         {gamePhase !== 'done' && <StageSelectionList stages={stages} action={action} getDisableState={getDisableState} />}
         {gamePhase === 'done' && <ChosenStageView stage={chosenStage} />}
       </section>
-      <BaseButton onClick={reset}>
+      <BaseButton onClick={handleNextGame}>
         {chosenStage ? 'Next game' : 'Reset'}
       </BaseButton>
     </>
