d1508e8253a1026c4678eae0911b147ef844b23e
klaymant <clement.gauthier@protonmail.com>
2024-06-17 18:26:51 +0200

    Limited content width on big screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <AppProvider>
       <StagesProvider>
-        <main className="flex justify-around flex-wrap gap-x-4 p-4 sm:p-8">
+        <main className="p-4 sm:p-8">
           <section>
             <PhaseSelector />
             <StageSelection />
