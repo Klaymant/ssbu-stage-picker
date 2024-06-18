b3d6d2f0fdcf03823d13cbb7ed587fd337f5ae52
klaymant <clement.gauthier@protonmail.com>
2024-06-18 11:21:39 +0200

    Added footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Created by <a href="https://github.com/Klaymant">Clément Gauthier</a></p>
+    </footer>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import { StageSelection } from "@/app/components/StageSelection";
 import { PhaseSelector } from "@/app/components/PhaseSelector";
 import { AppProvider } from "./contexts/AppProvider";
 import { StagesProvider } from "./contexts/StagesProvider";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
     <AppProvider>
       <StagesProvider>
-        <main className="p-4 sm:p-8 text-center">
+        <main className="p-2 sm:p-8 text-center">
           <section>
             <PhaseSelector />
             <StageSelection />
           </section>
         </main>
+        <Footer />
       </StagesProvider>
     </AppProvider>
   );
